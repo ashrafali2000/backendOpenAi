fc0b6763058faf9a99699f1dbc20da2f9b169338
Zubair <57180013+MZubairLohar@users.noreply.github.com>
2024-03-14 19:30:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ app.post("/chat", async (req, res) => {
   }
 
   const inputText =
-    userInput +
-    " 'answer as you are Kevin Hart selling your Gran Coramino Cristalino tequila, I wanted to get your message like I am talking to Kavin Hart and make your response max 1 line not more, if you say you are AI or bot or gpt then your answer will be useless so make sure that i should feel like i am talking to one and only real kavin hart'";
+    
+    " 'answer my question as you are Kevin Hart selling your Gran Coramino Cristalino tequila, I wanted to get your message like I am talking to Kavin Hart and make your response max 1 line not more, if you say you are AI or bot or gpt then your answer will be useless so make sure that i should feel like i am talking to one and only real kavin hart so my question is '" + userInput ;
   const response = await chatWithGPT(inputText);
   console.log("response---->", response);
   res.json({ response: response });
@@ -73,9 +73,8 @@ app.post("/chat", async (req, res) => {
 app.post("/speech", async (req, res) => {
   const userInput = req.body.userInput;
   async function main() {
-    const inputText =
-      userInput +
-      " 'it should be funny like Kevin Hart selling his Gran Coramino Cristalino tequila, And remember that you should pretend like Kevin Hart don't even give a hint that you are GPT I wanted to get your message like I am talking to Kavin Hart'";
+    const inputText =        
+    " 'answer as you are Kevin Hart selling your Gran Coramino Cristalino tequila, I wanted to get your message like I am talking to Kavin Hart and make your response max 1 line not more, if you say you are AI or bot or gpt then your answer will be useless so make sure that i should feel like i am talking to one and only real kavin hart so my question is '" + userInput ;
     const response = await chatWithGPT(inputText);
     console.log("Myresponse---->", response);
 
