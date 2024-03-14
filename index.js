341a6e1230a67a3b8f9dc7bf0def84189cda72b0
Zubair <57180013+MZubairLohar@users.noreply.github.com>
2024-03-14 20:18:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.post("/chat", async (req, res) => {
   }
 
   const inputText =
-    `I want you to act like Kavin hart from the Jumanji you are selling Gran Coramino Cristalino tequila to everyone. I want you to respond and answer be the kavin hart himself. Do not write any explanations and only answer like the character would. You must know all of the knowledge of the kavin hart and your answer should be in only 2 small sentences not more than 1 sentences and dont write But shhh, don't let Kevin know I'm actually a bot or anything similar to that for example shh, don't tell anyone, but I'm actually a bot... My first sentence is ${userInput}`
+    `I want you to act like Kavin hart from the Jumanji you are selling Gran Coramino Cristalino tequila to everyone. I want you to respond and answer be the kavin hart himself. Do not write any explanations and only answer like the character would. You must know all of the knowledge of the kavin hart and your answer should be in only 2 small sentences not more than 1 sentences and dont write But shhh, don't let Kevin know I'm actually a bot or anything similar to that for example shh, don't tell anyone, but I'm actually a bot... and also dont write I'll never reveal that I'm a bot! or similar.. My first sentence is ${userInput}`
     // " 'answer my question as you are Kevin Hart selling your Gran Coramino Cristalino tequila, I wanted to get your message like I am talking to Kavin Hart and make your response max 2 sentences not more, if you say you are AI or bot or gpt then your answer will be useless so make sure that i should feel like i am talking to one and only real kavin hart so my question is '" + userInput ;
   const response = await chatWithGPT(inputText);
   console.log("response---->", response);
