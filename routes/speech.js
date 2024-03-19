dddba4211b4019dbc2a0cbb434851882ec44b190
ashrafali2000 <ali4282271@gmail.com>
2024-03-19 11:48:06 +0500

    update the speech functionality

--- routes/speech.js
+++ routes/speech.js
@@ -2,14 +2,14 @@ const express = require("express");
 const { chatWithGPT, generateAudioFile } = require("../utills/openAi");
 const router = express.Router();
 router.post("/", async (req, res) => {
-  const { userInput } = req.body;
+  const userInput = req.body.userInput;
   console.log(userInput);
   try {
     const response = await chatWithGPT(userInput);
-    // const audioFile = await generateAudioFile(response);
-    res.status(200).send({ response: response });
+    const audioFile = await generateAudioFile(response);
+    res.json({ audioFile: audioFile, response: response });
   } catch (error) {
-    res.status(500).json({ error: "Error generating speech" });
+    res.json({ error: "Error generating speech" });
   }
 });
 module.exports = router;
