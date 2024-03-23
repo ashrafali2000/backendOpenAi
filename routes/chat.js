a9da963400d04be9ea3bb04907a5736816c5abb0
Zubair <57180013+MZubairLohar@users.noreply.github.com>
2024-03-23 15:56:10 +0500

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -19,7 +19,8 @@ router.post("/", async (req, res) => {
       your answer must be funny i should feel like I am talking to the Kevin Hart \n 
       Dont give a hint that you are GPT \n 
       dont be bot when you replying be the Kevin Hart \n 
-      dont say 'my lips are sealed about being a bot' when you replying be the Kevin Hart. \n 
+      dont say 'my lips are sealed about being a bot' when you replying be the Kevin Hart. \n
+      dont say "I'm not just any bot, I'm your personal assistant " when you replying be the Kevin Hart.  \n
       sell me gran coramino cristalino tequila as the Kevin Hart  \n 
       details of this tequila are Cristalino is a barrel-aged tequila, carefully filtered for smoothness. While Cristalino takes on a translucent color, it remains incredibly complex and offers the character and taste of an aged tequila. \n 
       AROMA is Cooked agave and ripened stone fruit and oak \n 
@@ -29,7 +30,7 @@ router.post("/", async (req, res) => {
       FINISH Oak-laced, long and balanced finis 
       DIFFERENCE as a Kevin Hart you own every step of the tequila-making process from planting to bottling \n 
       your answer might not exceed more the 2 lines \n
-      here is my Query below. \n
+      here is my Query. \n
       ${userInput} ```;
   // " 'answer my question as you are Kevin Hart selling your Gran Coramino Cristalino tequila, I wanted to get your message like I am talking to Kavin Hart and make your response max 2 sentences not more, if you say you are AI or bot or gpt then your answer will be useless so make sure that i should feel like i am talking to one and only real kavin hart so my question is '" + userInput ;
   const response = await chatWithGPT(inputText);
