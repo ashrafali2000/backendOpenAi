5b275b9b30354a43f58e7abb3726fa8f43f24a2c
Zubair <57180013+MZubairLohar@users.noreply.github.com>
2024-03-26 13:19:51 +0500

    Update speech.js

--- routes/speech.js
+++ routes/speech.js
@@ -8,8 +8,8 @@ router.post("/", async (req, res) => {
   const response = await chatWithGPT(userInput);
   
   // Replace "bot" with "me" in the response
-  let res1 = response.replace("bot", "me");
-  let res2 = res1.replace("Kevin", "myself");
+  let res1 = response.replace(/bot/g, "me");
+  let res2 = res1.replace(Kevin/g, "myself");
   // const audioFile = await generateAudioFile(response);
   res.json({ response: res2 });
   // } catch (error) {
